46a196521ba6bb13f7ea00b8081f3331bac70d82
Franklin Webber <franklin.webber@gmail.com>
2013-03-18 15:05:41 -0600

    Spec - cleaned up unused creation in Attribute Spec

--- spec/happymapper_attribute_spec.rb
+++ spec/happymapper_attribute_spec.rb
@@ -2,10 +2,6 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe HappyMapper::Attribute do
   describe "initialization" do
-    before do
-      @attr = HappyMapper::Attribute.new(:foo, String)
-    end
-
     it 'should accept :default as an option' do
       attr = described_class.new(:foo, String, :default => 'foobar')
       attr.default.should == 'foobar'
