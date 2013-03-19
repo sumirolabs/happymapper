17aece7b3544d19089d882fd643f6dbcfe1f4395
Franklin Webber <franklin.webber@gmail.com>
2013-03-19 09:10:57 -0600

    Updated specs for .parse and #parse

--- spec/parse_instance_spec.rb
+++ spec/parse_instance_spec.rb
@@ -1,129 +1,111 @@
 require 'spec_helper'
 
-parse_instance_initial_xml = %{
-  <root attr1="initial">
-    <item attr1="initial">
-      <description>initial</description>
-      <subitem attr1="initial">
-        <name>initial</name>
-      </subitem>
-      <subitem attr1="initial">
-        <name>initial</name>
-      </subitem>
-    </item>
-    <item attr1="initial">
-      <description>initial</description>
-      <subitem attr1="initial">
-        <name>initial</name>
-      </subitem>
-      <subitem attr1="initial">
-        <name>initial</name>
-      </subitem>
-    </item>
-  </root>
-}
+describe "Updating existing objects with .parse and #parse" do
 
-parse_instance_updated_xml = %{
-  <root attr1="updated">
-    <item attr1="updated">
-      <description>updated</description>
-      <subitem attr1="updated">
-        <name>updated</name>
-      </subitem>
-      <subitem attr1="updated">
-        <name>updated</name>
-      </subitem>
-    </item>
-    <item attr1="updated">
-      <description>updated</description>
-      <subitem attr1="updated">
-        <name>updated</name>
-      </subitem>
-      <subitem attr1="updated">
-        <name>updated</name>
-      </subitem>
-    </item>
-  </root>
-}
+  let(:subject) { ParseInstanceSpec::Root.parse(parse_instance_initial_xml) }
 
-module ParseInstanceSpec
-  class SubItem
-    include HappyMapper
-    tag 'subitem'
-    attribute :attr1, String
-    element :name, String
+  let(:parse_instance_initial_xml) do
+     %{<root attr1="initial">
+      <item attr1="initial">
+        <description>initial</description>
+        <subitem attr1="initial">
+          <name>initial</name>
+        </subitem>
+        <subitem attr1="initial">
+          <name>initial</name>
+        </subitem>
+      </item>
+      <item attr1="initial">
+        <description>initial</description>
+        <subitem attr1="initial">
+          <name>initial</name>
+        </subitem>
+        <subitem attr1="initial">
+          <name>initial</name>
+        </subitem>
+      </item>
+    </root>}
   end
-  class Item
-    include HappyMapper
-    tag 'item'
-    attribute :attr1, String
-    element :description, String
-    has_many :sub_items, SubItem
+
+  let(:parse_instance_updated_xml) do
+    %{<root attr1="updated">
+      <item attr1="updated">
+        <description>updated</description>
+        <subitem attr1="updated">
+          <name>updated</name>
+        </subitem>
+        <subitem attr1="updated">
+          <name>updated</name>
+        </subitem>
+      </item>
+      <item attr1="updated">
+        <description>updated</description>
+        <subitem attr1="updated">
+          <name>updated</name>
+        </subitem>
+        <subitem attr1="updated">
+          <name>updated</name>
+        </subitem>
+      </item>
+    </root>}
   end
-  class Root
-    include HappyMapper
-    tag 'root'
-    attribute :attr1, String
-    has_many :items, Item
+
+  module ParseInstanceSpec
+    class SubItem
+      include HappyMapper
+      tag 'subitem'
+      attribute :attr1, String
+      element :name, String
+    end
+    class Item
+      include HappyMapper
+      tag 'item'
+      attribute :attr1, String
+      element :description, String
+      has_many :sub_items, SubItem
+    end
+    class Root
+      include HappyMapper
+      tag 'root'
+      attribute :attr1, String
+      has_many :items, Item
+    end
   end
+
+  def item_is_correctly_defined(item,value='initial')
+    expect(item.attr1).to eq value
+    expect(item.description).to eq value
+    expect(item.sub_items[0].attr1).to eq value
+    expect(item.sub_items[0].name).to eq value
+    expect(item.sub_items[1].attr1).to eq value
+    expect(item.sub_items[1].name).to eq value
+  end
+
+  it 'initial values are correct' do
+    subject.attr1.should == 'initial'
+    item_is_correctly_defined( subject.items[0] )
+    item_is_correctly_defined( subject.items[1] )
+  end
+
+
+  describe ".parse", "specifying an existing object to update" do
+    it 'all fields are correct' do
+      ParseInstanceSpec::Root.parse(parse_instance_updated_xml, :update => subject)
+      expect(subject.attr1).to eq 'updated'
+
+      item_is_correctly_defined( subject.items[0], 'updated' )
+      item_is_correctly_defined( subject.items[1], 'updated' )
+    end
+  end
+
+  describe "#parse" do
+    it "all fields are correct" do
+      subject.parse(parse_instance_updated_xml)
+      expect(subject.attr1).to eq 'updated'
+
+      item_is_correctly_defined( subject.items[0], 'updated' )
+      item_is_correctly_defined( subject.items[1], 'updated' )
+    end
+  end
+
 end
-
-describe HappyMapper do
-  describe "update existing instance by parsing new xml" do
-
-    it 'should have initial values' do
-      @initial.attr1.should == 'initial'
-      @initial.items[0].attr1.should == 'initial'
-      @initial.items[0].description.should == 'initial'
-      @initial.items[0].sub_items[0].attr1.should == 'initial'
-      @initial.items[0].sub_items[0].name.should == 'initial'
-      @initial.items[0].sub_items[1].attr1.should == 'initial'
-      @initial.items[0].sub_items[1].name.should == 'initial'
-      @initial.items[1].attr1.should == 'initial'
-      @initial.items[1].description.should == 'initial'
-      @initial.items[1].sub_items[0].attr1.should == 'initial'
-      @initial.items[1].sub_items[0].name.should == 'initial'
-      @initial.items[1].sub_items[1].attr1.should == 'initial'
-      @initial.items[1].sub_items[1].name.should == 'initial'
-    end
-
-    it 'should have updated values' do
-      ParseInstanceSpec::Root.parse(parse_instance_updated_xml, :update => @initial)
-      @initial.attr1.should == 'updated'
-      @initial.items[0].attr1.should == 'updated'
-      @initial.items[0].description.should == 'updated'
-      @initial.items[0].sub_items[0].attr1.should == 'updated'
-      @initial.items[0].sub_items[0].name.should == 'updated'
-      @initial.items[0].sub_items[1].attr1.should == 'updated'
-      @initial.items[0].sub_items[1].name.should == 'updated'
-      @initial.items[1].attr1.should == 'updated'
-      @initial.items[1].description.should == 'updated'
-      @initial.items[1].sub_items[0].attr1.should == 'updated'
-      @initial.items[1].sub_items[0].name.should == 'updated'
-      @initial.items[1].sub_items[1].attr1.should == 'updated'
-      @initial.items[1].sub_items[1].name.should == 'updated'
-    end
-
-    it "should be able to update instance from 'parse()' instance method" do
-      @initial.parse(parse_instance_updated_xml)
-      @initial.attr1.should == 'updated'
-      @initial.items[0].attr1.should == 'updated'
-      @initial.items[0].description.should == 'updated'
-      @initial.items[0].sub_items[0].attr1.should == 'updated'
-      @initial.items[0].sub_items[0].name.should == 'updated'
-      @initial.items[0].sub_items[1].attr1.should == 'updated'
-      @initial.items[0].sub_items[1].name.should == 'updated'
-      @initial.items[1].attr1.should == 'updated'
-      @initial.items[1].description.should == 'updated'
-      @initial.items[1].sub_items[0].attr1.should == 'updated'
-      @initial.items[1].sub_items[0].name.should == 'updated'
-      @initial.items[1].sub_items[1].attr1.should == 'updated'
-      @initial.items[1].sub_items[1].name.should == 'updated'
-    end
-
-    before(:each) do
-      @initial = ParseInstanceSpec::Root.parse(parse_instance_initial_xml)
-    end
-  end
-end
-
