f750aba01e6af8ca8a4103c5eda73ff8a2af7f5e
Matijs van Zuijlen <matijs@matijs.net>
2018-01-12 22:00:26 +0100

    Autocorrect Style/TernaryParentheses

--- lib/happymapper.rb
+++ lib/happymapper.rb
@@ -387,7 +387,7 @@ module HappyMapper
       end
 
       # Nothing matching found, we can go ahead and return
-      return ((options[:single] || root) ? nil : []) if nodes.size == 0
+      return (options[:single] || root ? nil : []) if nodes.size == 0
 
       # If the :limit option has been specified then we are going to slice
       # our node results by that amount to allow us the ability to deal with
