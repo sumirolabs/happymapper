88cebce859ddfdad6da90739fc9f9d99cf99e9c3
Matijs van Zuijlen <matijs@matijs.net>
2018-03-17 17:31:19 +0100

    Collect coverage information

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@
 source 'http://rubygems.org'
 
 gemspec
+
+if ENV['CI']
+  gem 'coveralls', group: :development if ENV['TRAVIS_RUBY_VERSION'] == '2.5'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  track_files 'lib/**/*.rb'
+  add_filter '/spec/'
+end
+
+if ENV['CI']
+  begin
+    require 'coveralls'
+    Coveralls.wear!
+  rescue LoadError
+    nil
+  end
+end
+
 require 'rspec'
 
 require 'happymapper'
