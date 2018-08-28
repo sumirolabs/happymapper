e503054931e9671a77174947655c6ac6fa9a2ce5
Matijs van Zuijlen <matijs@matijs.net>
2018-08-28 15:48:57 +0200

    Simplify Integer type handling

--- spec/happymapper/item_spec.rb
+++ spec/happymapper/item_spec.rb
@@ -79,17 +79,46 @@ describe HappyMapper::Item do
       end
     end
 
-    it 'works with Integers' do
-      item = described_class.new(:foo, Integer)
-      [21, 21.0, '21'].each do |a|
-        expect(item.typecast(a)).to eq(21)
+    context 'with Integers' do
+      let(:item) { described_class.new(:foo, Integer) }
+
+      it 'works with an integer' do
+        expect(item.typecast(21)).to eq 21
+      end
+
+      it 'works with a float' do
+        expect(item.typecast(21.0)).to eq 21
+      end
+
+      it 'works with a string' do
+        expect(item.typecast('21')).to eq 21
+      end
+
+      it 'works with a string with trailing characters' do
+        expect(item.typecast('21foo')).to eq 21
+      end
+
+      it 'works with a string with zero plus trailing characters' do
+        expect(item.typecast('0foo')).to eq 0
+      end
+
+      it 'handles a non-numeric string' do
+        expect(item.typecast('foo')).to be_nil
+      end
+
+      it 'handles an empty string' do
+        expect(item.typecast('')).to be_nil
+      end
+
+      it 'handles nil' do
+        expect(item.typecast(nil)).to be_nil
       end
     end
 
     it 'works with Floats' do
       item = described_class.new(:foo, Float)
       [21, 21.0, '21'].each do |a|
-        expect(item.typecast(a)).to eq(21.0)
+        expect(item.typecast(a)).to eq 21.0
       end
     end
 
@@ -98,19 +127,20 @@ describe HappyMapper::Item do
       expect(item.typecast('2000-01-01 01:01:01.123456')).to eq(Time.local(2000, 1, 1, 1, 1, 1, 123_456))
     end
 
-    it 'works with Dates' do
-      item = described_class.new(:foo, Date)
-      expect(item.typecast('2000-01-01')).to eq(Date.new(2000, 1, 1))
-    end
+    context 'with Date' do
+      let(:item) { described_class.new(:foo, Date) }
 
-    it 'handles nil Dates' do
-      item = described_class.new(:foo, Date)
-      expect(item.typecast(nil)).to eq(nil)
-    end
+      it 'works with a string' do
+        expect(item.typecast('2000-01-01')).to eq(Date.new(2000, 1, 1))
+      end
 
-    it 'handles empty string Dates' do
-      item = described_class.new(:foo, Date)
-      expect(item.typecast('')).to eq(nil)
+      it 'handles nil' do
+        expect(item.typecast(nil)).to be_nil
+      end
+
+      it 'handles empty string' do
+        expect(item.typecast('')).to eq(nil)
+      end
     end
 
     context 'with DateTime' do
